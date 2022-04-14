2b69a5f347f5660eacce1a0b774cb4b41a85d1b0
Syed Baqir Ali <gitbaq@gmail.com>
2022-04-14 21:51:54 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import * as React from "react";
 
-function App() {
+import CssBaseline from "@mui/material/CssBaseline";
+import Feed from "./components/feed";
+import Sidebar from "./components/sidebar";
+import Widgets from "./components/widgets";
+import { ThemeProvider } from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
+import { iswapTheme } from "./theme";
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    display: "flex",
+    justifyContent: "center",
+    height: "100vh",
+    width: "100vw",
+    background: "#15202b",
+  },
+  content: {
+    display: "flex",
+    justifyContent: "space-around",
+
+    maxWidth: "1200px",
+    background: "#FFEEEE",
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={iswapTheme}>
+        <div className={classes.wrapper}>
+          <div className={classes.content}>
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/stats/index.jsx
@@ -0,0 +1,42 @@
+// import './App.css';
+
+import { TrendingUp } from "@mui/icons-material";
+import { Box } from "@mui/system";
+
+const Stats = () => {
+  return (
+    <Box
+      sx={{
+        bgcolor: "background.paper",
+        boxShadow: 1,
+        borderRadius: 1,
+        p: 2,
+        minWidth: 300,
+      }}
+    >
+      <Box sx={{ color: "text.secondary" }}>Sessions</Box>
+      <Box sx={{ color: "text.primary", fontSize: 34, fontWeight: "medium" }}>
+        98.4 K
+      </Box>
+      <Box
+        component={TrendingUp}
+        sx={{ color: "success.dark", fontSize: 16, verticalAlign: "sub" }}
+      />
+      <Box
+        sx={{
+          color: "success.dark",
+          display: "inline",
+          fontWeight: "medium",
+          mx: 0.5,
+        }}
+      >
+        18.77%
+      </Box>
+      <Box sx={{ color: "text.secondary", display: "inline", fontSize: 12 }}>
+        vs. last week
+      </Box>
+    </Box>
+  );
+};
+
+export default Stats;
